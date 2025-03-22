9b5faffe9ad4e46880b43e86a19c67c2dacbf103
djoolz <jojuble@proton.me>
2025-03-22 21:45:50 +0100

    necessary ports are now delcared for each module individually

--- nixos/firewall.nix
+++ nixos/firewall.nix
@@ -5,7 +5,7 @@
   # https://nixos.wiki/wiki/Firewall
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 24800 22000 ];
+    # allowedTCPPorts = [ 24800 22000 ];
     # allowedUDPPorts = [69]; # TFTP, for flashing openwrt to fritzbox routers
     # allowedUDPPortRanges = [
     #   { from = 4000; to = 4007; }
