361b896a6ab382abceae450d7c548bb2a6836d17
djoolz <jojuble@proton.me>
2025-03-22 20:40:57 +0100

    efi specific bootloader

--- nixos/bootloader.nix
+++ nixos/bootloader.nix
@@ -1,13 +1,16 @@
 {
   # Bootloader.
   boot = {
+
     loader = {
-      grub = {
-        enable = true;
-        device = "/dev/sda";
-        useOSProber = true;
-        };
-      };
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    #   grub = {
+    #     enable = true;
+    #     device = "/dev/sda";
+    #     useOSProber = true;
+    #     };
+    };
 
     # Set resume device to enable hibernating.
     resumeDevice = "/dev/disk/by-label/swap";
