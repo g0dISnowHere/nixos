c4e34873df211c8b24c66553fcf3eba128d69217
djoolz <jojuble@proton.me>
2025-03-23 10:13:27 +0100

    clean up of podman.nix

--- nixos/podman.nix
+++ nixos/podman.nix
@@ -2,8 +2,8 @@
 {pkgs, ... }:
 {
   # Enable common container config files in /etc/containers
-  virtualisation.containers.enable = true;
   virtualisation = {
+    containers.enable = true;
     podman = {
       enable = true;
 
