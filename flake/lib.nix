39b6f8e9fa33f3bd27dff2b7f23c0ec5c5135b1d
g0disnowhere <jojuble@gmail.com>
2026-03-28 12:42:35 +0100

    home-manager backup command

--- flake/lib.nix
+++ flake/lib.nix
@@ -30,11 +30,39 @@ in {
 
           # Home-manager integration
           home-manager.nixosModules.home-manager
+          ({ pkgs, ... }: {
+            home-manager.backupCommand =
+              pkgs.writeShellScript "home-manager-backup" ''
+                set -eu
+
+                target_path="$1"
+                backup_root="$HOME/.local/state/home-manager-backups"
+                timestamp="$(date +%Y%m%d-%H%M%S-%N)"
+
+                case "$target_path" in
+                  "$HOME"/*)
+                    relative_path="''${target_path#$HOME/}"
+                    ;;
+                  *)
+                    relative_path="external/$(basename "$target_path")"
+                    ;;
+                esac
+
+                backup_dir="$backup_root/$(dirname "$relative_path")"
+                backup_name="$(basename "$target_path").$timestamp"
+                backup_path="$backup_dir/$backup_name"
+
+                mkdir -p "$backup_dir"
+                mv "$target_path" "$backup_path"
+
+                find "$backup_root" -type f -mtime +30 -delete
+                find "$backup_root" -depth -type d -empty -delete
+              '';
+          })
           {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
-              backupFileExtension = "hm-backup";
               # Note: Individual machines set home-manager.users.*
               # in their default.nix to reference profile modules
               extraSpecialArgs = {
