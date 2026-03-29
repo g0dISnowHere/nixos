30767840cb57bf23162389adbdfd4d96302da1dc
g0disnowhere <jojuble@gmail.com>
2026-03-29 16:13:13 +0200

    alhena ssh pubkey

--- modules/nixos/users/djoolz/ssh.nix
+++ modules/nixos/users/djoolz/ssh.nix
@@ -1,6 +1,7 @@
 { ... }: {
   users.users.djoolz.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJG4h30pgt87nkKeNeF1qNtFv9cj7QkjqD76sZQEiFSf djoolz@centauri-2026-03-28"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILIXA/w+q0MsmIJ8a8HhE4M+qGZbXd8gkpJ4kGPZB48Y djoolz@mirach-2026-03-29"    
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILIXA/w+q0MsmIJ8a8HhE4M+qGZbXd8gkpJ4kGPZB48Y djoolz@mirach-2026-03-29"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFMyJqO4mgeAs/Gxu/1dTrs7FJ1ox2Kzfw29FB5aCkW djoolz@alhena-2026-03-29"
   ];
 }
