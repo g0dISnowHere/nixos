8e827154feac675004bba4cc4f4132cac815348f
g0disnowhere <jojuble@gmail.com>
2026-04-03 12:11:05 +0200

    nix fmt

--- modules/nixos/system/disko-install-test-compat.nix
+++ modules/nixos/system/disko-install-test-compat.nix
@@ -1,12 +1,4 @@
-{
-  config,
-  extendModules,
-  inputs,
-  lib,
-  modulesPath,
-  pkgs,
-  ...
-}:
+{ config, extendModules, inputs, lib, modulesPath, pkgs, ... }:
 let
   # Disko still calls qemu-common with { lib, stdenv }, but nixpkgs 25.11 now
   # expects { lib, pkgs }. Adapt the old call shape just for installTest.
@@ -15,19 +7,13 @@ let
     rootMountPoint = config.disko.rootMountPoint;
     makeTest = import "${modulesPath}/../tests/make-test-python.nix";
     eval-config = import "${modulesPath}/../lib/eval-config.nix";
-    qemu-common =
-      {
-        lib,
-        stdenv,
-        ...
-      }:
+    qemu-common = { lib, stdenv, ... }:
       import "${modulesPath}/../lib/qemu-common.nix" {
         inherit lib;
         pkgs = { inherit stdenv; };
       };
   };
-in
-{
+in {
   system.build.installTest = lib.mkForce (diskoLibCompat.testLib.makeDiskoTest {
     inherit extendModules pkgs;
     name = "${config.networking.hostName}-disko";
