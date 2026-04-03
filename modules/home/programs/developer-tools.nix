10f58278f41d1c19ef46d82921727df3f1089d51
g0disnowhere <jojuble@gmail.com>
2026-04-03 12:13:30 +0200

    update developer tools packages

--- modules/home/programs/developer-tools.nix
+++ modules/home/programs/developer-tools.nix
@@ -1,44 +1,43 @@
 { pkgs, pkgs-unstable, desktop ? null, ... }: {
-  home.packages = with pkgs; [
-    # Python
-    python3
-    python3Packages.pip
-    python3Packages.setuptools
-    python3Packages.wheel
-    python3Packages.virtualenv
-    python3Packages.pipx
-    uv
+  home.packages = with pkgs;
+    [
+      # Python
+      python3
+      python3Packages.pip
+      python3Packages.setuptools
+      python3Packages.wheel
+      python3Packages.virtualenv
+      python3Packages.pipx
+      uv
 
-    # Go
-    go
-    gopls
-    go-tools
+      # Go
+      go
+      gopls
+      go-tools
 
-    # JavaScript / Node.js
-    nodejs
-    npm-check-updates
-    yarn
-    pnpm
+      # JavaScript / Node.js
+      nodejs
+      npm-check-updates
+      yarn
+      pnpm
 
-    # Rust
-    rustc
-    cargo
-    rust-analyzer
-    clippy
-    rustfmt
+      # Rust
+      rustc
+      cargo
+      rust-analyzer
+      clippy
+      rustfmt
 
-    # Native build tooling
-    pkgconf
-    gcc
-    gnumake
-    cmake
+      # Native build tooling
+      pkgconf
+      gcc
+      gnumake
+      cmake
 
-    # Developer-focused newer packages
-    pkgs-unstable.devenv
-    pkgs-unstable.gh
-  ] ++ pkgs.lib.optionals (desktop != null) [
-    pkgs-unstable.vscode
-  ];
+      # Developer-focused newer packages
+      pkgs-unstable.devenv
+      pkgs-unstable.gh
+    ] ++ pkgs.lib.optionals (desktop != null) [ pkgs-unstable.vscode ];
 
   home.sessionVariables = {
     GOPATH = "$HOME/go";
