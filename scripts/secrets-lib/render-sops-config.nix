f057fb77e7b9c171ce8713f7ab6f4588098928ce
g0disnowhere <jojuble@gmail.com>
2026-04-05 22:13:48 +0200

    fix multi-operator sops rendering

--- scripts/secrets-lib/render-sops-config.nix
+++ scripts/secrets-lib/render-sops-config.nix
@@ -1,6 +1,15 @@
 let
   policy = import ../../flake/secrets-policy.nix;
   sort = builtins.sort builtins.lessThan;
+  operatorRecipientAliases = let
+    recipients = policy.operator.recipients;
+    count = builtins.length recipients;
+    mkAlias = index:
+      if index == 0 then
+        policy.operator.alias
+      else
+        "${policy.operator.alias}_${toString index}";
+  in map mkAlias (builtins.genList (index: index) count);
   renderAgeList = aliases:
     builtins.concatStringsSep "\n"
     (map (alias: "          - *${alias}") aliases);
@@ -16,16 +25,17 @@ let
     in builtins.concatStringsSep "\n" (map (name:
       let hosts = sort scopes.${name}.hosts;
       in renderRule "^secrets/${scopeName}/${name}/.*\\.${extensionPattern}$"
-      ([ policy.operator.alias ] ++ hosts)) names);
+      (operatorRecipientAliases ++ hosts)) names);
   renderMachineRules = builtins.concatStringsSep "\n" (map (host:
-    renderRule "^secrets/machines/${host}/.*\\.(yaml|json|env|ini)$" [
-      policy.operator.alias
-      host
-    ]) (sort (builtins.attrNames policy.hosts)));
-  keys = [
-    (renderKeyLine policy.operator.alias
-      (builtins.head policy.operator.recipients))
-  ] ++ map (host: renderKeyLine host policy.hosts.${host}.recipient)
+    renderRule "^secrets/machines/${host}/.*\\.(yaml|json|env|ini)$"
+    (operatorRecipientAliases ++ [ host ]))
+    (sort (builtins.attrNames policy.hosts)));
+  operatorKeys = builtins.genList (index:
+    renderKeyLine (builtins.elemAt operatorRecipientAliases index)
+    (builtins.elemAt policy.operator.recipients index))
+    (builtins.length policy.operator.recipients);
+  keys = operatorKeys
+    ++ map (host: renderKeyLine host policy.hosts.${host}.recipient)
     (sort (builtins.attrNames policy.hosts));
 in builtins.concatStringsSep "\n" [
   "keys:"
